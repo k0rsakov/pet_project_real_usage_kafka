29381c005d3429a1db3c676e6d12769f52a850c5
k0rsakov <korsakov.iyu@gmail.com>
2025-08-28 11:16:30 +0300

    Изменён проект pet_project_real_usage_kafka: - Добавлен новый пакет - Добавлен простой kafka producer

---
+++ code/simple_producer.py
@@ -0,0 +1,49 @@
+import json
+import time
+
+import uuid_utils as uuid
+from confluent_kafka import Producer
+from faker import Faker
+
+
+def generate_list_of_dict() -> dict[str, str]:
+
+    fake = Faker(locale="ru_RU")
+
+    return {
+        "uuid": str(uuid.uuid7()),
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "middle_name": fake.middle_name(),
+    }
+
+
+# Define the Kafka configuration
+conf = {"bootstrap.servers": "localhost:19092"}
+
+# Create a Producer instance with the above configuration
+producer = Producer(conf)
+
+# simple RPS
+interval = 1.0 / 1
+
+while True:
+    start = time.perf_counter()
+    # Define some data to send to Kafka
+    data = generate_list_of_dict()
+
+    # Convert the data to a JSON string
+    data_str = json.dumps(data)
+    print(f"Event sent: {data_str}\n\n")
+
+    # Produce a message to the "my_topic" topic
+    producer.produce(topic="my_topic", value=data_str)
+
+    # Flush the producer to ensure all messages are sent
+    producer.flush()
+
+    # Sleep for a second before producing the next set of messages
+
+    elapsed = time.perf_counter() - start
+    sleep_time = max(0, interval - elapsed)
+    time.sleep(sleep_time)
